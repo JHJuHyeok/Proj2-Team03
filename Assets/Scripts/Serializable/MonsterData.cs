c208904df7ecd83d49fdfc9ada08fbb5794618c0
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-01-30 17:28:14 +0900

    feat : 몬스터 데이터 변환용 직렬화 스크립트 작성

---
+++ Assets/Scripts/Serializable/MonsterData.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using System.Numerics;
+using System.Collections.Generic;
+
+public class MonsterDataList
+{
+    public List<MonsterData> monsterList;
+}
+
+[System.Serializable]
+public class MonsterData
+{
+    public string id;                   // 몬스터 ID
+    public string name;                 // 몬스터 명칭
+    public string spriteName;           // 이미지 이름
+
+    public BigInteger maxHp;            // 체력
+    public BigInteger Attack;           // 공격력
+
+    public MonsterType type;            // 몬스터 타입
+    public SkillElement weakElement;    // 약점 속성
+}
+
+public enum MonsterType
+{
+    normal,
+    Boss
+}
