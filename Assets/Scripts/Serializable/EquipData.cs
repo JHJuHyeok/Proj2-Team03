202bbe9e054fb78d7cd176ce69b3781619151f35
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-01-31 17:56:32 +0900

    feat : 장비 Json 데이터 생성용 직렬화 스크립트 작성

---
+++ Assets/Scripts/Serializable/EquipData.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class EquipDataList
+{
+    public List<EquipData> equipList;
+}
+
+public enum EquipType
+{
+    Weapon,
+    Accessorie
+}
+
+[System.Serializable]
+public class EquipData
+{
+    public string id;                           // 장비 ID
+    public string name;                         // 장비 명칭
+    public string spriteName;                   // 이미지 이름
+
+    public EquipGrade grade;                    // 장비 등급
+    public ItemEffect equipEffect;              // 장착 효과
+    public List<ItemEffect> holdEffects;        // 보유 효과 목록
+}
+
+public enum EquipGrade
+{
+    Common,
+    Uncommon,
+    Rare,
+    Hero,
+    Legend,
+    Myth,
+    Infinite
+}
+
+[System.Serializable]
+public class ItemEffect
+{
+    public EffectType type;     // 효과 종류
+    public float value;         // 수치
+}
+
+public enum EffectType
+{
+    AttackBoost,        // 공격력 증가 (%)
+    CriticalDamage,     // 추가 치명타 데미지
+    GoldGain,           // 추가 골드 획득량
+    HealthBoost,        // 체력/체력 회복량 증가
+    ManaBoost,          // 전체 마나/마나 회복량
+    ExpGain             // 추가 경험치
+}
