482d73f8e4090ca0ae16a4622ee4320fec3eb9b5
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-02-04 18:30:25 +0900

    feat : 아틀라스 스프라이트 매니저 제작

---
+++ Assets/Scripts/Managers/Resource/SpriteManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.AddressableAssets;
+using UnityEngine.U2D;
+using System.Threading.Tasks;
+
+public class SpriteManager : Singleton<SpriteManager>
+{
+    // Dictionary<호출 아틀라스 명칭, 아틀라스>
+    private Dictionary<string, SpriteAtlas> _atlasCache = new();
+
+    public async Task<Sprite> GetSprite(string atlasAddress, string spriteName)
+    {
+        // 이미 로드된 아틀라스가 있는지 확인
+        if (!_atlasCache.TryGetValue(atlasAddress, out SpriteAtlas atlas))
+        {
+            // 없다면 어드레서블로 아틀라스 로드
+            AsyncOperationHandle<SpriteAtlas> handle = Addressables.LoadAssetAsync<SpriteAtlas>(atlasAddress);
+            await handle.Task;
+
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                atlas = handle.Result;
+                _atlasCache[atlasAddress] = atlas;
+            }
+            else
+            {
+                Debug.Log("아틀라스 로드 실패");
+                return null;
+            }
+        }
+
+        Sprite targetSprite = atlas.GetSprite(spriteName);
+
+        if (targetSprite == null)
+        {
+            Debug.Log("아틀라스에 해당하는 스프라이트가 없습니다.");
+        }
+
+        return targetSprite;
+    }
+}
