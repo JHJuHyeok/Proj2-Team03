b5595afb099a61ae981bb610c07dc738c9b20220
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-02-12 15:44:00 +0900

    refactor : DataManager 정적 클래스로 변경, 정리

--- Assets/Scripts/Managers/Data/DataManager.cs
+++ Assets/Scripts/Managers/Data/DataManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public enum GameDataType
 {
@@ -14,44 +15,48 @@ public enum GameDataType
     Gacha
 }
 
-public class DataManager : Singleton<DataManager>
+public static class DataManager
 {
-    // 실시간 데이터 임시 보관
-    public GameData currentSaveData { get; private set; }
+    // 실시간 유저 데이터
+    public static GameData CurrentSaveData { get; private set; }
 
-    public GameDB<MonsterData, MonsterDataList> monsters = new();
-    public GameDB<SkillData, SkillDataList> skills = new();
-    public GameDB<EquipData, EquipDataList> weapons = new();
-    public GameDB<EquipData, EquipDataList> accessories = new();
-    
-    // StageList actually contains AreaData
-    public GameDB<AreaData, StageDataList> maps = new();
+    // 읽기 전용 데이터베이스
+    public static readonly GameDB<MonsterData, MonsterDataList> monsters = new();
+    public static readonly GameDB<SkillData, SkillDataList> skills = new();
+    public static readonly GameDB<EquipData, EquipDataList> weapons = new();
+    public static readonly GameDB<EquipData, EquipDataList> accessories = new();
+    public static readonly GameDB<AreaData, StageDataList> stages = new();
 
+    // UI 이벤트 전달
     public static event Action<GameDataType, string> OnDataUpdated;
 
-    public void Init(GameData data)
+    public static void Init(GameData data)
     {
-        // 현재 데이터 초기화
-        currentSaveData = data;
+        // 데이터 동기화
+        CurrentSaveData = data;
 
         Debug.Log("성장 데이터 동기화 완료");
     }
 
-    public void LoadAllDatabase()
+    /// <summary>
+    /// 비동기 데이터베이스 구성
+    /// </summary>
+    /// <returns></returns>
+    public static async Task LoadAllDatabase()
     {
-        monsters.Load("Json/Monster/MonsterList");
-        skills.Load("Json/Skill/SkillList");
-        weapons.Load("Json/Equip/WeaponList");
-        accessories.Load("Json/Equip/AccessorieList");
-        
-        maps.Load("Json/Stage/StageList");
-        
+        await Task.WhenAll(
+            monsters.LoadAsync("Json/Monster/MonsterList"),
+            skills.LoadAsync("Json/Skill/SkillList"),
+            weapons.LoadAsync("Json/Equip/WeaponList"),
+            accessories.LoadAsync("Json/Equip/AccessorieList"),
+            stages.LoadAsync("Json/Stage/StageList")
+        );
         Debug.Log("데이터 로드 완료");
     }
 
-    public StageData GetStage(string stageId)
+    public static StageData GetStage(string stageId)
     {
-        foreach (var area in maps.GetAll())
+        foreach (var area in stages.GetAll())
         {
             if (area.stageList != null)
             {
@@ -66,9 +71,9 @@ public class DataManager : Singleton<DataManager>
     }
 
     // 스테이지 ID로 해당 지역 데이터 반환
-    public AreaData GetAreaByStageId(string stageId)
+    public static AreaData GetAreaByStageId(string stageId)
     {
-        foreach (var area in maps.GetAll())
+        foreach (var area in stages.GetAll())
         {
             if (area.stageList != null)
             {
@@ -80,5 +85,15 @@ public class DataManager : Singleton<DataManager>
             }
         }
         return null;
+    }
+
+    /// <summary>
+    /// 데이터 타입별 이벤트 반환
+    /// </summary>
+    /// <param name="type"> 데이터 타입 </param>
+    /// <param name="id"> 변경되는 요소 ID </param>
+    public static void TriggerDataUpdate(GameDataType type, string id)
+    {
+        OnDataUpdated?.Invoke(type, id);
     }
 }
